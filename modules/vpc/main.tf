4856d22d006a5493958cd8a966680ae481f9703e
Anirudha5997 <144744094+Anirudha5997@users.noreply.github.com>
2024-02-21 20:57:09 -0500

    tf-gcp (#3)
    * terraform-gcp
    
    * a3 branch
    
    * assign3
    
    * Update main.tf
    
    * Update main.tf
    
    * Update main.tf
    
    * Update main.tf
    
    * tf-gcp

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -45,5 +45,114 @@ resource "google_compute_route" "route" {
   network          = each.value.network
   next_hop_gateway = each.value.next_hop_gateway
 }
+ 
+resource "google_compute_firewall" "firewall" {
+  for_each = {
+    for idx, config in flatten([
+      for vpc_name, config in var.vpc : flatten([for firewall, firewall_config in config.firewalls :
+        {
+          firewall_name = firewall_config.firewall_name
+          network = google_compute_network.vpc_network[vpc_name].id
+          allow = firewall_config.allow
+          source_tags = firewall_config.source_tags
+        }])
 
+    ]) : idx => config
+  }
+
+  name    = each.value.firewall_name
+  network = each.value.network
+  source_tags = each.value.source_tags
+
+  dynamic "allow" {
+    for_each = each.value.allow
+    content {
+      protocol = allow.value.protocol
+      ports = allow.value.ports
+    }
+  }
+}
+
+# dynamic "google_service_account" {
+#   for_each = var.service_account
+#   content{
+
+#   }
+# }
+
+resource "google_compute_instance" "vm" {
+    for_each = var.vm-properties
+  
+    name         = each.value.name
+    machine_type = each.value.machine_type
+    can_ip_forward      = each.value.can_ip_forward
+    zone = each.value.zone
+
+    dynamic "boot_disk" {
+      for_each = tolist([each.value.boot_disk])
+
+      content {
+        auto_delete = boot_disk.value.auto_delete
+        device_name = boot_disk.value.device_name
+        mode = boot_disk.value.mode
+
+        dynamic "initialize_params" {
+          for_each = tolist([boot_disk.value.initialize_params])
+
+          content {
+            image = initialize_params.value.image
+            size  = initialize_params.value.size
+            type  = initialize_params.value.type
+          }
+        }
+      }
+    }
+
+    dynamic "network_interface" {
+      for_each = each.value.network_interface
+      content {
+        queue_count = network_interface.value.queue_count
+        stack_type  = network_interface.value.stack_type
+        subnetwork  = network_interface.value.subnetwork
+
+        dynamic "access_config" {
+            for_each = tolist([network_interface.value.access_config])
+            content {
+              network_tier = access_config.value.network_tier
+            }
+        }
+      }
+    }
+
+    dynamic "scheduling" {
+        for_each = each.value.scheduling
+
+        content {
+            automatic_restart   = scheduling.value.automatic_restart
+            on_host_maintenance = scheduling.value.on_host_maintenance
+            preemptible         = scheduling.value.preemptible
+            provisioning_model  = scheduling.value.provisioning_model
+        }
+      
+    }
+
+    dynamic "service_account" {
+        for_each = each.value.service_account
+        
+        content {
+            email  = service_account.value.email
+            scopes = service_account.value.scopes
+        }
+    }
+
+    dynamic "shielded_instance_config" {
+        for_each = each.value.shielded_instance_config
+
+        content {
+            enable_integrity_monitoring = shielded_instance_config.value.enable_integrity_monitoring
+            enable_secure_boot          = shielded_instance_config.value.enable_secure_boot
+            enable_vtpm                 = shielded_instance_config.value.enable_vtpm
+        }
+    }
+}
 
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -15,6 +15,77 @@ variable "vpc" {
       destination_range = string
       next_hop_gateway  = string
     }))
+
+    firewalls = map(object({
+      firewall_name = string
+
+      allow = list(object({
+        protocol = string
+        ports = list(string)
+      }))
+      
+      source_tags = list(string)
+    }))
+  }))
+}
+
+
+variable "service_account" {
+  type = object({
+    account_id = string
+    display_name = string
+  })
+}
+
+variable "vm-properties" {
+  description = "Configuration(s) for VMs"
+  type = map(object({
+    name                = string
+    machine_type        = string
+    zone                = string
+    can_ip_forward      = bool
+
+
+    boot_disk = object({
+      auto_delete = bool
+      device_name = string
+      mode        = string
+
+      initialize_params = object({
+        image = string
+        size  = number
+        type  = string
+      })
+    })
+
+    network_interface = map(object({
+      queue_count = number
+      stack_type  = string
+      subnetwork  = string
+
+      access_config = object({
+        network_tier = string
+      })
+    }))
+
+    scheduling = map(object({
+      automatic_restart   = bool
+      on_host_maintenance = string
+      preemptible         = bool
+      provisioning_model  = string
+    }))
+
+    service_account = map(object({
+      email  = string
+      scopes = list(string)
+    }))
+
+    shielded_instance_config = map(object({
+      enable_integrity_monitoring = bool
+      enable_secure_boot          = bool
+      enable_vtpm                 = bool
+    }))
+
   }))
 }
 
